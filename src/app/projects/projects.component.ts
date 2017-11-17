b74e8d5a8f36ab61dae2556c112943ed79402de8
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-11-17 00:59:04 -0500

    Imported project class

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project';
 
 @Component({
   selector: 'app-projects',
@@ -7,11 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectsComponent implements OnInit {
 
+  project: Project = {
+    id: 1,
+    name: 'Home Chef'
+    technologies: ['NodeJS', 'React', 'Redux']
+  }
+
   constructor() { }
 
   ngOnInit() {
   }
-
-  project = 'okay-chatty';
 
 }
