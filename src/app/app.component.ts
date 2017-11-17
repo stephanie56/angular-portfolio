9baf6d2a102c64dac2cd5ddc7da65b6e03f9ddda
Stephanie Zeng <stephaniezeng521@gmail.com>
2017-11-17 00:35:30 -0500

    Set up index page: hero an about sections

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Code + Impact';
+  subtitle = 'Design is thinking made visible.';
+  about = 'Hi there! The name is Stephanie. I\'m a hands-on and experienced Full-Stack Developer who has a passion about making interesting apps, from designing, developing to deploying.';
+  job = 'Full-Stack Developer + Javascript';
+  image = 'https://avatars2.githubusercontent.com/u/14033992?v=3&s=460';
+}
